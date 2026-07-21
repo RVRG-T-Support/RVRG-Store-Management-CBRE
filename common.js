ce249a7aee0e4460fdb37a0182537999593ad869
Tech-RVRG <technicalsupport@rvrg.in>
2026-07-21 21:53:51 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 // common.js - Core Utilities & Navigation
 
 // Safe initialization using 'var' to prevent redeclaration crashes
-var supabase = window.supabaseClient || window.supabase;
+var supabase = window.supabaseDb;
 
 // 1. Session & Auth Management
 function getCurrentUser() {
